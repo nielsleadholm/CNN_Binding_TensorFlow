aa6b0528209c6d0217b7e9cbb45be9e0445630ff
nielsleadholm <n.leadholm@gmail.com>
2019-07-12 15:56:08 +0100

    Improve handling of distance metrics

--- tfCore_adversarial_attacks.py
+++ tfCore_adversarial_attacks.py
@@ -42,154 +42,197 @@ class parent_attack:
 
         #Define the class attribute, attack_method, to be the Blended Uniform Noise attack by default
         attack_method = foolbox.attacks.BlendedUniformNoiseAttack 
-        distance_metric = scipy.spatial.distance.euclidean
-        attack_type_dir = 'ParentClass_attack_images'
+        foolbox_distance_metric = foolbox.distances.MeanSquaredDistance
+        attack_type_dir = 'Parent_*_not_advised_*_'
 
-        def evaluate_resistance(self, distance_metric=distance_metric, attack_type_dir=attack_type_dir):
+        def evaluate_resistance(self):
 
-                logits = self.model_prediction_function(self.input_placeholder, self.dropout_rate_placeholder, self.weights_dic, self.biases_dic)
-                saver = tf.train.Saver(self.var_list) #Define saver object for use later when loading the model weights
+            logits = self.model_prediction_function(self.input_placeholder, self.dropout_rate_placeholder, self.weights_dic, self.biases_dic)
+            saver = tf.train.Saver(self.var_list) #Define saver object for use later when loading the model weights
 
-                with tf.Session() as session:
-                        saver.restore(session, self.model_weights) #Note when restoring weights its important not to run init on the same
-                        #variables, as this will over-write the learned weights with randomly initialized ones
-                        #Define the foolbox model
-                        fmodel = foolbox.models.TensorFlowModel(self.input_placeholder, logits, (0,1)) 
+            with tf.Session() as session:
+                    saver.restore(session, self.model_weights) #Note when restoring weights its important not to run init on the same
+                    #variables, as this will over-write the learned weights with randomly initialized ones
+                    #Define the foolbox model
+                    fmodel = foolbox.models.TensorFlowModel(self.input_placeholder, logits, (0,1)) 
 
-                        print("Evaluating " + str(self.num_attack_examples) + " adversarial example(s)")
+                    print("\nPerforming " + self.attack_type_dir + " attack(s)")
+                    print("Evaluating " + str(self.num_attack_examples) + " adversarial example(s)")
 
-                        #Arrays for storing results of the evaluation
-                        adversary_found = np.zeros([self.num_attack_examples]) #array of booleans that indicates if an adversary was found for a particular image
-                        adversary_distance = np.zeros([self.num_attack_examples])
-                        adversary_arrays = np.zeros([self.num_attack_examples, self.input_data.shape[1], self.input_data.shape[2], 1])
+                    #Arrays for storing results of the evaluation
+                    adversary_found = np.zeros([self.num_attack_examples]) #array of booleans that indicates if an adversary was found for a particular image
+                    adversary_distance = np.zeros([self.num_attack_examples])
+                    adversary_arrays = np.zeros([self.num_attack_examples, self.input_data.shape[1], self.input_data.shape[2], 1])
 
-                        for ii in range(self.num_attack_examples):
+                    for ii in range(self.num_attack_examples):
 
-                                execution_data = self.input_data[ii, :, :]
-                                execution_label = np.argmax(self.input_labels[ii,:])
+                            execution_data = self.input_data[ii, :, :]
+                            execution_label = np.argmax(self.input_labels[ii,:])
 
-                                #Check the predicted label of the network prior to carrying out the attack isn't already incorrect
-                                pre_label = np.argmax(fmodel.predictions(execution_data[:, :, :]))
-                                if (pre_label != execution_label):
-                                    print("The model predicted a " + str(pre_label) + " when the ground-truth label is " + str(execution_label))
+                            #Check the predicted label of the network prior to carrying out the attack isn't already incorrect
+                            pre_label = np.argmax(fmodel.predictions(execution_data[:, :, :]))
+                            if (pre_label != execution_label):
+                                print("The model predicted a " + str(pre_label) + " when the ground-truth label is " + str(execution_label))
 
-                                #Carry out the attack
-                                self.attack_fmodel = self.attack_method(model=fmodel, criterion=self.criterion)
-                                adversarial_image_fmodel = self.__create_adversarial(execution_data, execution_label)
+                            #Carry out the attack
+                            self.attack_fmodel = self.attack_method(model=fmodel, criterion=self.criterion, distance=self.foolbox_distance_metric)
+                            adversarial_image_fmodel = self.create_adversarial(execution_data, execution_label)
+                            
+                            #Check the output of the adversarial attack
 
-
-                                
-                                #Check the output of the adversarial attack
-
-                                if np.any(adversarial_image_fmodel == None):
-                                    print("\n\n *** No adversarial image found *** \n\n")
-                                elif np.argmax(fmodel.predictions(adversarial_image_fmodel[:, :, :])) == execution_label:
-                                    print("\n\n *** No adversarial image found *** \n\n")
-                                else:
-                                    if os.path.exists('adversarial_images/' + self.output_directory + '/' + attack_type_dir + '/') == 0:
+                            if np.any(adversarial_image_fmodel == None):
+                                print("\n\n *** No adversarial image found *** \n\n")
+                            elif np.argmax(fmodel.predictions(adversarial_image_fmodel[:, :, :])) == execution_label:
+                                print("\n\n *** No adversarial image found *** \n\n")
+                            else:
+                                if os.path.exists('adversarial_images/' + self.output_directory + '/' + self.attack_type_dir + '/') == 0:
+                                    try:
                                         os.mkdir('adversarial_images/' + self.output_directory + '/')
-                                        os.mkdir('adversarial_images/' + self.output_directory + '/' + attack_type_dir + '/')
-
-                                    plt.imsave('adversarial_images/' + self.output_directory + '/' + 
-                                        attack_type_dir + '/num' + str(ii) + '.png', adversarial_image_fmodel[:,:,0], cmap='gray')
+                                    except OSError:
+                                        pass
+                                    try:
+                                        os.mkdir('adversarial_images/' + self.output_directory + '/' + self.attack_type_dir + '/')
+                                    except OSError:
+                                        pass
                                     
-                                    adversary_arrays[ii, :, :] = adversarial_image_fmodel
 
-                                    print("The classification label following attack is " + str(np.argmax(fmodel.predictions(adversarial_image_fmodel[:, :, :]))) 
-                                            + " from an original classification of " + str(execution_label))
-                                    distance = distance_metric(execution_data.flatten(), adversarial_image_fmodel.flatten())
-                                    print("The " + str(distance_metric) + " distance of the adversary is " + str(distance))
-                                    adversary_found[ii] = 1
-                                    adversary_distance[ii] = distance
+                                plt.imsave('adversarial_images/' + self.output_directory + '/' + 
+                                    self.attack_type_dir + '/num' + str(ii) + '.png', adversarial_image_fmodel[:,:,0], cmap='gray')
                                 
-                        return adversary_found, adversary_distance, adversary_arrays
+                                adversary_arrays[ii, :, :] = adversarial_image_fmodel
+
+                                print("The classification label following attack is " + str(np.argmax(fmodel.predictions(adversarial_image_fmodel[:, :, :]))) 
+                                        + " from an original classification of " + str(execution_label))
+                                distance, distance_name = self.distance_metric(execution_data.flatten(), adversarial_image_fmodel.flatten())
+                                print("The " + distance_name + " distance of the adversary is " + str(distance))
+                                adversary_found[ii] = 1
+                                adversary_distance[ii] = distance
+                            
+                    return adversary_found, adversary_distance, adversary_arrays
 
 
-        def __create_adversarial(self, execution_data, execution_label):
+        def create_adversarial(self, execution_data, execution_label):
 
-                adversarial_image_fmodel = self.attack_fmodel(execution_data, execution_label)
-                return adversarial_image_fmodel
+            adversarial_image_fmodel = self.attack_fmodel(execution_data, execution_label)
+            return adversarial_image_fmodel
+
+        def distance_metric(self, vector1, vector2):
+            distance = scipy.spatial.distance.euclidean(vector1, vector2)
+            distance_name = 'Euclidean (L-2)'
+            return distance, distance_name
 
 
 class blended_noise_attack(parent_attack):
-        attack_type_dir = 'Blended_noise_attack_images'
+        attack_type_dir = 'Blended_noise'
         #As the parent attack already uses the blended uniform noise attack by default, no changes are necessary
 
-# class pointwise_attack(parent_attack):
-#         attack_method = foolbox.attacks.PointwiseAttack
-#         distance_metric = scipy.spatial.distance.hamming
-#         attack_type_dir = 'Pointwise_attack_images'
+class pointwise_attack(parent_attack):
+        attack_method = foolbox.attacks.PointwiseAttack
+        foolbox_distance_metric = foolbox.distances.L0 #This is the distance metric used during optimization by FoolBox attacks
+        attack_type_dir = 'Pointwise'
 
-# class boundary_attack(parent_attack):
-#         #Overwite parent constructor for two additional attributes : num_iterations and log_every_n_steps
-#         #As it is overwritten, it needs to be explicitly called here
-#         def __init__(self, model_prediction_function,
-#                         model_weights,
-#                         var_list,
-#                         input_data,
-#                         input_labels,
-#                         input_placeholder,
-#                         num_attack_examples=1, 
-#                         criterion=foolbox.criteria.Misclassification(), 
-#                         dropout_rate=0.0,
-#                         num_iterations=50,
-#                         log_every_n_steps=50):
-#                 parent_attack.__init__(self, model_prediction_function,
-#                         model_weights,
-#                         var_list,
-#                         input_data,
-#                         input_labels,
-#                         input_placeholder,
-#                         criterion=foolbox.criteria.Misclassification(), 
-#                         dropout_rate=0.0)
-#                 self.num_iterations = num_iterations
-#                 self.log_every_n_steps = log_every_n_steps
+        #This is the distance metric used to evalute the final distances of the returned images from the original
+        def distance_metric(self, vector1, vector2):
+            distance = scipy.spatial.distance.hamming(vector1, vector2)
+            distance_name = 'Hamming (L-0)'
+            return distance, distance_name
+
+class boundary_attack(parent_attack):
+        #Overwite parent constructor for two additional attributes : num_iterations and log_every_n_steps
+        #As it is overwritten, it needs to be explicitly called here
+        def __init__(self, model_prediction_function,
+                        model_weights,
+                        var_list,
+                        weights_dic,
+                        biases_dic,
+                        input_data,
+                        input_labels,
+                        input_placeholder,
+                        dropout_rate_placeholder,
+                        output_directory,
+                        num_attack_examples=3, 
+                        criterion=foolbox.criteria.Misclassification(), 
+                        dropout_rate=0.0,
+                        num_iterations=50,
+                        log_every_n_steps=50):
+                parent_attack.__init__(self, model_prediction_function,
+                        model_weights,
+                        var_list,
+                        weights_dic,
+                        biases_dic,
+                        input_data,
+                        input_labels,
+                        input_placeholder,
+                        dropout_rate_placeholder,
+                        output_directory,
+                        num_attack_examples, 
+                        criterion, 
+                        dropout_rate)
+                self.num_iterations = num_iterations
+                self.log_every_n_steps = log_every_n_steps
         
-#         attack_method = foolbox.attacks.BoundaryAttack
-#         attack_type_dir = 'Boundary_attack_images'
+        attack_method = foolbox.attacks.BoundaryAttack
+        attack_type_dir = 'Boundary'
 
-#         #Overwrite the execute attack method, as the boundary attack requires a specified number of iterations
-#         def __create_adversarial(self, execution_data, execution_label):
-#                 adversarial_image_fmodel = self.attack_fmodel(self.input_data, self.input_labels, iterations=self.num_iterations, log_every_n_steps=self.log_every_n_steps)
+        #Overwrite the execute attack method, as the boundary attack requires a specified number of iterations
+        def create_adversarial(self, execution_data, execution_label):
 
-#                 return adversarial_image_fmodel
+                adversarial_image_fmodel = self.attack_fmodel(execution_data, execution_label, iterations=self.num_iterations, 
+                    log_every_n_steps=self.log_every_n_steps, verbose=False)
 
-# class BIM_L2_attack(parent_attack):
-#         def __init__(self, model_prediction_function,
-#                         model_weights,
-#                         var_list,
-#                         input_data,
-#                         input_labels,
-#                         input_placeholder,
-#                         num_attack_examples=1, 
-#                         criterion=foolbox.criteria.Misclassification(), 
-#                         dropout_rate=0.0,
-#                         epsilon=0.3):
-#                 parent_attack.__init__(self, model_prediction_function,
-#                         model_weights,
-#                         var_list,
-#                         input_data,
-#                         input_labels,
-#                         input_placeholder,
-#                         criterion=foolbox.criteria.Misclassification(), 
-#                         dropout_rate=0.0)
-#                 self.epsilon = epsilon
+                return adversarial_image_fmodel
 
-#         #Inherit the attributes of the BIM_Linfinity_attack class, then overwite the attack method
-#         attack_method = foolbox.attacks.L2BasicIterativeAttack
-#         attack_type_dir = 'BIM2_attack_images'
+class BIM_L2_attack(parent_attack):
+        def __init__(self, model_prediction_function,
+                        model_weights,
+                        var_list,
+                        weights_dic,
+                        biases_dic,
+                        input_data,
+                        input_labels,
+                        input_placeholder,
+                        dropout_rate_placeholder,
+                        output_directory,
+                        num_attack_examples=3, 
+                        criterion=foolbox.criteria.Misclassification(), 
+                        dropout_rate=0.0,
+                        epsilon=0.3):
+                parent_attack.__init__(self, model_prediction_function,
+                        model_weights,
+                        var_list,
+                        weights_dic,
+                        biases_dic,
+                        input_data,
+                        input_labels,
+                        input_placeholder,
+                        dropout_rate_placeholder,
+                        output_directory,
+                        num_attack_examples, 
+                        criterion, 
+                        dropout_rate)
+                self.epsilon = epsilon
 
-#         def __create_adversarial(self, execution_data, execution_label):
-#                 adversarial_image_fmodel = self.attack_fmodel(self.input_data, self.input_labels, epsilon=self.epsilon)
+        #Inherit the attributes of the BIM_Linfinity_attack class, then overwite the attack method
+        attack_method = foolbox.attacks.L2BasicIterativeAttack
+        attack_type_dir = 'BIM2'
 
-#                 return adversarial_image_fmodel
+        def create_adversarial(self, execution_data, execution_label):
+
+                adversarial_image_fmodel = self.attack_fmodel(execution_data, execution_label, epsilon=self.epsilon)
+                return adversarial_image_fmodel
 
 
-# class BIM_Linfinity_attack(BIM_L2_attack):
-#         #Inherit the attributes of the BIM_L2_attack class, then overwite the attack method and summary distance metric
-#         attack_method = foolbox.attacks.LinfinityBasicIterativeAttack
-#         distance_metric = scipy.spatial.distance.chebyshev
-#         attack_type_dir = 'BIMInf_attack_images'
+class BIM_Linfinity_attack(BIM_L2_attack):
+        #Inherit the attributes of the BIM_L2_attack class, then overwite the attack method and summary distance metric
+        attack_method = foolbox.attacks.LinfinityBasicIterativeAttack
+        foolbox_distance_metric = foolbox.distances.Linfinity
+        attack_type_dir = 'BIMInf'
+
+        def distance_metric(self, vector1, vector2):
+
+            distance = scipy.spatial.distance.chebyshev(vector1, vector2)
+            distance_name = 'Chebyshev (L-Inf)'
+            return distance, distance_name
 
 
 
